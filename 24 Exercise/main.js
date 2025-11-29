664f7520b07f41a2ff2079dc943e5766c6e90ea6
fuadmn <dayib716@gmail.com>
2025-11-29 17:16:04 +0300

    loop es6

---
+++ 24 Exercise/main.js
@@ -0,0 +1,14 @@
+
+function greet( name = "Guest") {
+  console.log(`Hello ${name}`);
+}
+
+greet();
+
+
+function calculateArea(width,height){
+  console.log(width * height)
+  
+}
+
+calculateArea()
